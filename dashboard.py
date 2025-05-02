f493b72781ecaad5bb6d92994817c57413aa1914
Vidiya Dawah <you@example.com>
2025-05-01 21:04:27 -0400

    Made changes to the dashboard

--- dashboard.py
+++ dashboard.py
@@ -22,11 +22,7 @@ conn_str2= (
 conn1 = pyodbc.connect(conn_str) #Connects to the PatientDatabase
 cursor1 = conn1.cursor()
 
-conn2 = pyodbc.connect(conn_str2)
+conn2 = pyodbc.connect(conn_str2) #Connects to the Profile database
 cursor2 = conn2.cursor()
 
-cursor1.execute("SELECT Name FROM MSysObjects WHERE Type=1 AND Flags=0;")
-tables = cursor1.fetchall()
-for table in tables:
-    print(table[0])
 #create a dashboard to show the data analytics
