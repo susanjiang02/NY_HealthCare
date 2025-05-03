e60bd808cd39ffcc9aa9a4ad7279c5fb140de259
Vidiya Dawah <you@example.com>
2025-05-03 02:12:02 -0400

    Delete feature works properly

--- app.py
+++ app.py
@@ -32,11 +32,11 @@ def register():
 def index():
     conn1 = pyodbc.connect(conn_str)
     cursor1 = conn1.cursor()
-    cursor1.execute("SELECT PatientName, AppointmentDate, AppointmentTime, DoctorName, ReasonForVisit FROM Appointment")
+    cursor1.execute("SELECT AppointmentID, PatientName, AppointmentDate, AppointmentTime, DoctorName, ReasonForVisit FROM Appointment")
     rows = cursor1.fetchall()
-    conn1.close()
+    # conn1.close()
 
-    appointments = [
+    appointments = [  #gets appt info
         {
             "patient": row.PatientName,
             "date": row.AppointmentDate,
@@ -45,17 +45,24 @@ def index():
             "reason": row.ReasonForVisit
         } for row in rows
     ]
-    return render_template('appointments.html', appointments= appointments)
+        #get doctor info
+    cursor1.execute("SELECT LastName FROM Doctor")
+    doctor_rows = cursor1.fetchall()
+    doctors = [row.LastName for row in doctor_rows]
+    conn1.close()
+
+    return render_template('appointments.html', appointments= appointments, doctors=doctors)
 
 
 
 @app.route('/book', methods=['POST'])
 def book_appointment():
-    patient_name = request.form.get('patient')
-    appointment_date = request.form.get('date')
-    appointment_time = request.form.get('time')
-    doctor_name = request.form.get('doctor')
-    reason_for_visit = request.form.get('reason')
+    data = request.get_json()
+    patient_name = data.get('patient')
+    appointment_date = data.get('date')
+    appointment_time = data.get('time')
+    doctor_name = data.get('doctor')
+    reason_for_visit = data.get('reason')
 
     conn1 = pyodbc.connect(conn_str)
     cursor1 = conn1.cursor()
@@ -68,17 +75,18 @@ def book_appointment():
     conn1.commit()
     conn1.close()
 
-    return redirect('/appointments')
+    return jsonify({"message": "Appointment has been booked."}), 200
 
 @app.route('/api/appointments')
 def view_all_appointments():
     conn1 = pyodbc.connect(conn_str)
     cursor1 = conn1.cursor()
-    cursor1.execute("SELECT AppointmentDate, AppointmentTime, DoctorName, ReasonForVisit, PatientName FROM Appointment")
+    cursor1.execute("SELECT AppointmentID, AppointmentDate, AppointmentTime, DoctorName, ReasonForVisit, PatientName FROM Appointment")
     rows = cursor1.fetchall()
     conn1.close()
     appointments = [
         {
+            "AppointmentID": row.AppointmentID,
             "patient": row.PatientName,
             "date": row.AppointmentDate,
             "time": row.AppointmentTime,
@@ -86,8 +94,23 @@ def view_all_appointments():
             "reason": row.ReasonForVisit
         } for row in rows
     ]
-    
+    # print(appointments)
     return jsonify(appointments)
+
+@app.route('/delete/<int:appointment_id>', methods=['DELETE'])
+def delete_appt(appointment_id):
+    conn1 = pyodbc.connect(conn_str)
+    cursor1 = conn1.cursor()
+    try:
+        cursor1.execute("DELETE FROM Appointment WHERE AppointmentID = ?", (appointment_id,))
+        conn1.commit()
+        return '', 204
+    except Exception as e:
+        print('Was not able to delete the appointment.', e)
+        return 'Could not delete', 500
+    finally:
+        conn1.close()
+   
 
 if __name__=='__main__':
     app.run(debug=True, host='0.0.0.0')
